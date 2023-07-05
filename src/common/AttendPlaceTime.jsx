2110d8dc7292bfc074a2385e1122957f13a917fb
Isami-Kobayashi <135718343+Isami-Kobayashi@users.noreply.github.com>
2023-07-05 22:28:23 +0900

    firestore追加

--- src/common/AttendPlaceTime.jsx
+++ src/common/AttendPlaceTime.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { InputLabel, MenuItem, FormControl, Select } from "@mui/material";
 
 export const AttendPlaceTime = (props) => {
+  const saveDB = async (event) => {
+    event.preventDefault();
+    const { type1 } = event.target.elements;
+    console.log(type1.value);
+  };
   return (
     <FormControl>
       <InputLabel></InputLabel>
-      <Select>
+      <Select onChange={saveDB}>
         {props.options.map((option) => (
-          <MenuItem key={option.id} value={option.id}>
+          <MenuItem name="type1" key={option.id} value={option.id}>
             {option.name}
           </MenuItem>
         ))}
